d7285bb60b6aef1443254b1f2e0f33b124ef7db8
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-26 03:56:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -222,7 +222,7 @@ def most_points_scored
         curr = title[:points]
         if curr > max_points
           max_points = curr
-          max_name = :player_name
+          max_name = 
         end
       end
     end
